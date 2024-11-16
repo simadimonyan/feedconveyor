a4c9b9bfafad5d444308c7b58a55a2d0ccfd3d2f
Dimitri Simonyan <simadimonyan@gmail.com>
2024-11-16 23:20:27 +0300

    test

--- src/database/db.py
+++ src/database/db.py
@@ -1,48 +1,30 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import ChatOllama
-from langchain_postgres import PGVector
-from langchain_core.documents import Document
+from langchain_milvus import Milvus
 from dotenv import load_dotenv
 import os
-import json
 
 class Database:
 
     def __init__(self):
         load_dotenv(".env")
-        name = os.getenv("POSTGRES_NAME")
-        pwd = os.getenv("POSTGRES_PASSWORD")
+        server = os.getenv("MILVUS_HOST")
+        name = os.getenv("MILVUS_USER")
+        pwd = os.getenv("MILVUS_PASSWORD")
         ollama_url = os.getenv("OLLAMA_BASE_URL")
         ollama_model = os.getenv("OLLAMA_MODEL")
         self.ollama = ChatOllama(model=ollama_model, base_url=ollama_url)
-
-        connection = f"postgresql+psycopg://{name}:{pwd}@postgresql-pgvector:5432/feedconveyor" 
         embeddings = OllamaEmbeddings(model=ollama_model, base_url=ollama_url)
-        
-        self.vector_database = PGVector(
-            collection_name="store",
-            connection=connection,
-            embeddings=embeddings
-        )
-        self.vector_database.create_vector_extension()
-        self.vector_database.create_tables_if_not_exists()
 
+        self.vector_store = Milvus(
+            embedding_function=embeddings,
+            connection_args={"uri": server},
+            collection_name="store"
+        )
 
     async def store_data(self, data):
-
-        for doc in data:
-            if not isinstance(doc.metadata, dict):
-                raise ValueError("Metadata must be a dictionary")
-        
-        self.vector_database.add_documents(data, ids=[doc.metadata["id"] for doc in data])
+        self.vector_store.aadd_documents(data)
 
     def search(self, search):
-        docs: dict[Document] = self.vector_database.similarity_search(search)
-
-        for doc in docs:
-            if not isinstance(doc.metadata, dict):
-                raise ValueError("Retrieved metadata must be a dictionary")
-
-        return docs
-
+        pass
 
